a36ff60a39933f90151c57052d812872f47e81b3
Remus-Antonio <ekro98@gmail.com>
2020-03-07 16:30:51 +0200

    Camera basic follower
    Script so that the camera follows a given target every frame

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private Transform cameraFollow;
+    private float minSpeed = 1.5f;
+    private float currSpeed = 2f;
+    
+
+    void Start()
+    {
+        if (transform.CompareTag("MainCamera"))
+        {
+            cameraFollow = transform;
+        }
+    }
+
+    void Update()
+    {
+        KeepInBounds();
+    }
+
+    void KeepInBounds()
+    {
+        Vector3 currPos = cameraFollow.position;
+        float distance = Vector2.Distance(currPos, target.position);
+
+        currSpeed = 1.5f * distance < minSpeed ? minSpeed : 1.5f * distance;
+
+        Vector3 moveStep = Vector3.MoveTowards(new Vector3(currPos.x, currPos.y, target.position.z),
+            target.position, currSpeed * Time.deltaTime);
+        
+        cameraFollow.position = new Vector3(moveStep.x, moveStep.y, currPos.z);
+    }
+}
