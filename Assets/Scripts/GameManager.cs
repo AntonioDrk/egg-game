ac05883e09cede4ab89e4d5a4f9c868ac55a4429
Ema Hîrhui <emahirhui@gmail.com>
2020-04-13 12:19:03 +0300

    Added stars (more particles), points and Game Manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; set; }
+    public int Points { get; set; }
+
+    private void Awake()
+    {
+        Instance = this;
+        Points = 0;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(GameManager.Instance.Points);
+    }
+}
---
+++ Assets/Scripts/StarController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarController : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            GameManager.Instance.Points++;
+            Instantiate(Resources.Load<GameObject>("StarParticles") as GameObject, transform.position, Quaternion.identity);
+            Destroy(this.transform.parent.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+}
